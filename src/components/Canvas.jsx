f7845c6e8f0936607c6013fb13f0fa81021d9e6f
AakashProCoder <traniee.abhiwan01@gmail.com>
2024-02-26 18:07:23 +0530

    gravity

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -12,70 +12,35 @@ const WorldMap = () => {
   ];
   const canvasRef = useRef(null);
   const mouse = { x: undefined, y: undefined };
-  const drawRect = (ctx, count) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.filStyle = "green";
-    const delta = count % 800;
-    ctx.fillRect(10 + delta, 10, 100, 100);
-  };
-  const drawLine = (ctx, count) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    const delta = count % 800;
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 2;
-    console.log("drow hua");
-    ctx.beginPath();
-    ctx.moveTo(10 + delta, 50);
-    ctx.lineTo(20 + delta, 50);
-    ctx.stroke();
-  };
-  function Circle(x, y, ctx, dx, dy, raduis) {
-    // console.log(x);
+  // function randomColor(colors) {
+  //   return colors[colorArray[Math.floor(Math.random() * colors.length)]];
+  // }
 
+  function Circle(x, y, ctx, color, raduis, dx, dy, gravity) {
+    // console.log(x);
     this.y = y;
     this.x = x;
     this.dx = dx;
     this.dy = dy;
     this.ctx = ctx;
     this.raduis = raduis;
-    this.minRaduis = raduis;
-    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
-    // console.log("x=>", this.x, "y=>", this.y);
+    this.color = color;
     this.draw = function () {
-      // console.log(x, y);
-      this.ctx.fillStyle = this.color;
       this.ctx.beginPath();
-      this.ctx.lineWidth = 5;
       this.ctx.arc(this.x, this.y, this.raduis, 0, Math.PI * 2, false);
-
+      this.ctx.fillStyle = this.color;
       ctx.fill();
+      ctx.closePath();
     };
     this.update = function () {
-      if (
-        this.x + this.raduis >= this.ctx.canvas.width ||
-        this.x - this.raduis < 0
-      ) {
-        this.dx = -this.dx;
+      if (this.y + this.raduis > ctx.canvas.height) {
+        this.dy = -this.dy * 0.9;
+      } else {
+        this.dy += gravity;
       }
-      if (
-        this.y + this.raduis >= this.ctx.canvas.height ||
-        this.y - this.raduis < 0
-      ) {
-        this.dy = -this.dy;
-      }
-      this.x += this.dx;
+      console.log(this.y);
       this.y += this.dy;
-      //intrectivity
-      if (
-        mouse.x - this.x < 100 &&
-        mouse.x - this.x > -100 &&
-        mouse.y - this.y < 100 &&
-        mouse.y - this.y > -100
-      ) {
-        if (this.raduis < 100) this.raduis += 1;
-      } else if (this.raduis > this.minRaduis) {
-        this.raduis = this.raduis - 1;
-      }
+      this.draw();
     };
   }
   window.addEventListener("mousemove", (event) => {
@@ -90,39 +55,30 @@ const WorldMap = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-
-    let circles = [];
-    for (let i = 0; i < 800; i++) {
-      let redius = Math.random() * 3 + 5;
-      let x = Math.random() * (ctx.canvas.width - redius * 2) + redius;
-      let y = Math.random() * (ctx.canvas.height - redius * 2) + redius;
-      // console.log(x, y);
-      let dx = (Math.random() - 0.5) * 8;
-      let dy = (Math.random() - 0.5) * 8;
-      circles.push(new Circle(x, y, ctx, dx, dy, redius));
-    }
-    // console.log(circles);
-    // let x = Math.random() * ctx.canvas.width;
-    // let y = Math.random() * ctx.canvas.height;
-    //  console.log(x, y);
-    // let dx = (Math.random() - 0.5) * 8;
-    // let dy = (Math.random() - 0.5) * 8;
-    //  var circle = new Circle(x, y, ctx, dy, dx, redius);
-
+    let ball;
     let animationId;
+
+    function init() {
+      ball = new Circle(
+        ctx.canvas.width / 2,
+        ctx.canvas.height / 2,
+        ctx,
+        "red",
+        50,
+        1,
+        1,
+        1
+      );
+    }
 
     const render = () => {
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      // Reverse direction when reaching canvas boundaries
-      // circle.draw();
-      // circle.update();
+      ball.update();
       animationId = requestAnimationFrame(render);
-      for (let i = 0; i < circles.length; i++) {
-        circles[i].draw();
-        circles[i].update();
-      }
     };
+    init();
     render();
+
     return () => window.cancelAnimationFrame(animationId);
   }, []);
   return (
