55f6ea0677390ff1363183c6f4a438f77990fd24
AakashProCoder <traniee.abhiwan01@gmail.com>
2024-02-29 12:54:43 +0530

    applied elastic energy tranfer

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,3 +1,4 @@
+import { clear } from "@testing-library/user-event/dist/clear";
 import React, { useEffect, useRef } from "react";
 
 const WorldMap = () => {
@@ -7,7 +8,7 @@ const WorldMap = () => {
   useEffect(() => {
     const canvas = canvasRef.current;
     c = canvas.getContext("2d");
-
+    let animationId;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
@@ -25,10 +26,12 @@ const WorldMap = () => {
     };
 
     const handleResize = () => {
+      console.log(animationId);
+      window.cancelAnimationFrame(animationId);
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
-
       init();
+      animate();
     };
 
     //utility function
@@ -36,6 +39,47 @@ const WorldMap = () => {
       let xDistance = x1 - x2;
       let yDestance = y1 - y2;
       return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDestance, 2));
+    }
+    function rotate(velocity, angle) {
+      // console.log("velocity=>>>", velocity,"angle=>>" ,angle);
+      const rotatedVelocities = {
+        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle),
+      };
+      return rotatedVelocities;
+    }
+    function resolveCollision(particle, otherparticle) {
+      console.log("particale===>", particle, "otherparticle==>", otherparticle);
+      const xVelcityDiff = particle.velocity.x - otherparticle.velocity.x;
+      const yVelocityDiff = particle.velocity.y - otherparticle.velocity.y;
+      const xDist = otherparticle.x - particle.x;
+      const yDist = otherparticle.y - particle.y;
+      //prevent accedental overlap of particles
+      if (xVelcityDiff * xDist + yVelocityDiff * yDist >=0) {
+        // Grap angle between the two colliding particles
+        const angle = -Math.atan2(
+          otherparticle.y - particle.y,
+          otherparticle.x - particle.x
+        );
+        // store mass in var for better readablity in collison equation
+        const m1 = particle.mass;
+        const m2 = otherparticle.mass;
+        //velocity before equation
+        const u1 = rotate(particle.velocity, angle);
+        const u2 = rotate(otherparticle.velocity, angle);
+        // velocity after 1d collision equation
+        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
+        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+        //final velocity after rotating axis back to the origin location
+        const vFinal1 = rotate(v1, -angle);
+        const vFinal2 = rotate(v2, -angle);
+        //swap particlee velocities for realistic bounce
+        particle.velocity.x = vFinal1.x;
+        particle.velocity.y = vFinal1.y;
+
+        otherparticle.velocity.x = vFinal2.x;
+        otherparticle.velocity.y = vFinal2.y;
+      }
     }
     // Objects
     class Object {
@@ -45,56 +89,93 @@ const WorldMap = () => {
         this.radius = radius;
         this.color = color;
         this.distance = undefined;
+        this.mass = 1;
+        this.velocity = {
+          x: Math.random() - 0.5 + 1,
+          y: Math.random() - 0.5 + 1,
+        };
       }
 
       draw() {
         if (this.distance - 130 <= 0) {
           console.log("touch");
-          
         }
-        console.log(this.distance - 130);
+
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.fillStyle = this.color;
-
-        c.fill();
+        c.strokeStyle = "blue";
+        c.stroke();
         c.closePath();
       }
 
-      update() {
+      update(objects) {
         this.draw();
-        this.distance = getDistance(circle.x, circle.y, circle2.x, circle2.y);
+        for (let i = 0; i < objects.length; i++) {
+          if (this === objects[i]) {
+            continue;
+          }
+          if (
+            getDistance(this.x, this.y, objects[i].x, objects[i].y) -
+              this.radius * 2 <
+            0
+          ) {
+            console.log("collison detected");
+            resolveCollision(this, objects[i]);
+          }
+        }
+        // for boundries bounce back
+        if (
+          this.x + this.radius > window.innerWidth ||
+          this.x - this.radius <= 0
+        ) {
+          this.velocity.x = -this.velocity.x;
+        }
+        if (
+          this.y + this.radius > window.innerHeight ||
+          this.y - this.radius < 0
+        ) {
+          this.velocity.y = -this.velocity.y;
+        }
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
       }
     }
 
     // Implementation
-    let objects;
+    let object = [];
     let circle;
     let circle2;
     const init = () => {
-      circle = new Object(300, 300, 100, "black");
-      circle2 = new Object(undefined, undefined, 30, "red");
-      for (let i = 0; i < 400; i++) {
-        // objects.push()
+      object = [];
+      for (let i = 0; i < 100; i++) {
+        let radius = 10;
+        let x = Math.random() * (window.innerWidth - radius * 2) + radius;
+        let y = Math.random() * (window.innerHeight - radius * 2) + radius;
+
+        if (i !== 0) {
+          for (let j = 0; j < object.length; j++) {
+            // console.log(
+            //   getDistance(x, y, object[j].x, object[j].y) - radius * 2 < 0
+            // );
+
+            if (getDistance(x, y, object[j].x, object[j].y) - radius * 2 < 0) {
+              x = Math.random() * (window.innerWidth - radius * 2) + radius;
+              y = Math.random() * (window.innerHeight - radius * 2) + radius;
+              j = -1;
+            }
+          }
+        }
+        object.push(new Object(x, y, radius, "blue"));
       }
+      console.log(object);
     };
 
     // Animation Loop
+
     const animate = () => {
-      requestAnimationFrame(animate);
+      animationId = requestAnimationFrame(animate);
       c.clearRect(0, 0, canvas.width, canvas.height);
-      let distance=getDistance(circle.x,circle.y,circle2.x,circle2.y)
-      if (distance < circle.radius+circle2.radius) {
-        circle.color="green"
-        
-      }
-      else{
-        circle.color="black"
-      }
-      circle.update();
-      circle2.x = mouse.x;
-      circle2.y = mouse.y;
-      circle2.update();
+      object.forEach((particle) => particle.update(object));
     };
 
     // Event listeners
@@ -109,6 +190,8 @@ const WorldMap = () => {
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
       window.removeEventListener("resize", handleResize);
+      window.cancelAnimationFrame(animationId);
+      cancelAnimationFrame(animationId);
     };
   }, []);
 
